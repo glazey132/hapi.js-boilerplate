359eb41529cb7fd3f07b2399ee8ecc42b5276d69
Alex <alglaze132@gmail.com>
2018-07-13 00:23:21 -0700

    hapi server running

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.server({
+  host: 'localhost',
+  port: 8080
+});
+
+(async () => {
+  try {
+    await server.start();
+    console.log(`Hapi server started at: ${server.info.uri} (^_^)`);
+  } catch (err) {
+    console.log(err);
+  }
+})();
